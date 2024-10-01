74708863a3edf1767312b237b6622e60ef314855
FTPyreNova024 <jdaniel.mc4@gmail.com>
2024-10-01 14:49:17 -0400

    Created teh canvas and the basic variables of time and color and created the canvas.

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@
 
 "use strict";
 
+//Creates the starting hours and minutes of the clock.
+
+
+let hours = 12;
+let minutes = 0;
+
+//creates the two ends of the color spectrum for the background as arrays.
+
+const color1 = [161, 211, 255];
+const color2 = [26, 42, 255];
+
+/**
+ * Creates the canvas and sets the projets units to degrees.
+ */
+
+function setup() {
+    createCanvas(1000, 1000);
+    angleMode(DEGREES);
+}
